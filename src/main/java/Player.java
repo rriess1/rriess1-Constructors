32aab94c63c4f4b0b299f2afe72ee74783248700
rriess1 <robertwriess@yahoo.com>
2023-02-19 17:42:55 +0000

    first

--- src/main/java/Player.java
+++ src/main/java/Player.java
@@ -31,6 +31,7 @@ public class Player {
      * @param playerName the name of the player to be created.
      */
     public Player(String playerName){
+        this.playerName = playerName;
 
     }
 
@@ -48,6 +49,8 @@ public class Player {
      * @param level the starting level of the player to be created.
      */
     public Player(String playerName, int level){
+        this.playerName = playerName;
+        this.level = level;
 
     }
 
